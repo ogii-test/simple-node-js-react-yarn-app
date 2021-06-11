61ae765d973acb59c4b23b952fb1a25cb82ba0fc
Aaron Clark <ordinallx@gmail.com>
2021-06-11 14:48:40 +0900

    Delete jenkins directory

--- jenkins/scripts/kill.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-echo 'The following command terminates the "yarn start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
-set -x
-kill $(cat .pidfile)
